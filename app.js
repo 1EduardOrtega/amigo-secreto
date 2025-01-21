0ef3b3668a9ac21e06ca0236cbd7640797a914c8
Eduardo Ortega <2eduardortega@gmail.com>
2025-01-21 13:22:21 -0500

    creacion de array, funcion de actualizacion y validacion de datos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listadenombres = ["uno","dos","tres"];
+//console.log(listadenombres);
+
+
+
+function ActualizarLista(){
+    let nombreamigo = document.querySelector('#amigo').value;
+    document.querySelector('#listaAmigos').innerHTML += '<li>'+nombreamigo+'</li>';
+    limpiarCaja();
+
+}
+
+function agregarAmigo(){
+let nombreamigo = document.querySelector('#amigo').value;
+   if (nombreamigo == '') {
+
+    alert('Por favor, inserte un nombre.');
+   } else {
+
+    ActualizarLista();
+
+   }
+
+}
+
+
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
