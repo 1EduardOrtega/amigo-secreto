42e5753db5d0deb13ee186d65d08881589b3c8b2
Eduardo Ortega <2eduardortega@gmail.com>
2025-01-22 12:43:17 -0500

    Creación de la función de sorteo para el amigo secreto, validación de datos ingresados por el usuario y de campos, corrección de la lectura del array para mostrar en lista

--- app.js
+++ app.js
@@ -1,30 +1,66 @@
-let listadenombres = ["uno","dos","tres"];
-//console.log(listadenombres);
-
-
+let amigos = [];
+let nombreamigo
 
 function ActualizarLista(){
-    let nombreamigo = document.querySelector('#amigo').value;
-    document.querySelector('#listaAmigos').innerHTML += '<li>'+nombreamigo+'</li>';
-    limpiarCaja();
+   
+    //limpieza de la lista
+    document.querySelector('#listaAmigos').innerHTML = "";
 
+    //Se recorre el array para mostrar la lista
+    for (let i=0;i<amigos.length;i++){
+        document.querySelector('#listaAmigos').innerHTML += '<li>'+amigos[i]+'</li>';
+
+    }
+    limpiarCaja();
 }
 
 function agregarAmigo(){
-let nombreamigo = document.querySelector('#amigo').value;
-   if (nombreamigo == '') {
 
-    alert('Por favor, inserte un nombre.');
-   } else {
+    //lectura del dato ingresado por el usuario
+   nombreamigo = document.querySelector('#amigo').value;
 
-    ActualizarLista();
+   // se validan los datos, evitar campos vacios
+    if (nombreamigo == '') {
 
-   }
+        alert('Por favor, inserte un nombre.');
+       } else {
+        
+        // validacion de datos, los nombres no se repiten
+        if (amigos.includes(nombreamigo)){
+
+            alert('El nombre ya se encuentra en la lista, por favor inserte uno nuevo.')
+        } else {
+
+            //datos validados, se ingresan al array y se limpia el campo para nuevo dato
+            amigos.push(nombreamigo);
+            ActualizarLista();
+            
+        }
+        
+       }
 
 }
 
+function sortearAmigo(){
+
+        //se valida array con datos
+    if (amigos.length > 1) {
+
+        let numeroGenerado =  Math.floor(Math.random()*amigos.length);
+        document.querySelector('#listaAmigos').innerHTML = `El amigo secreto sorteado es: ${amigos[numeroGenerado]}`;
+        amigos = [];
+
+        // se valida que exista más de un nombre para poder sortear
+    } else {
+
+        alert ('Por favor, inserte un minimo de dos nombres para sortear.');
+    }
+    
 
 
+}
+
+// funcion para limpiar la caja
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
